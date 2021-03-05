6888430757b1464a8fdb1fc275efb6161a64dac6
Pankaj <pankajyadev63@gmail.com>
2021-03-05 10:45:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide up script
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed= new Typed(".typing", {
+        strings:["YouTuber","Developer","VideoCreator","Blogger","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true
+    });
+    var typed= new Typed(".typing-2", {
+        strings:["YouTuber","Developer","VideoCreator","Blogger","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+
+    });
+});
